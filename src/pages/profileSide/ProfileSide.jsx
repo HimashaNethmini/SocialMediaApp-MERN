72b1f33643745b504c8bd581492a6482dba52f86
HimashaNethmini <himashanethmini05@gmail.com>
2024-03-18 10:23:18 +0530

    initial stage of ProfileSide

---
+++ src/pages/profileSide/ProfileSide.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./profileSide.css"
+
+const ProfileSide = () => {
+  return (
+    <div className='ProfileSide'>hxjx
+    </div>
+  )
+}
+
+export default ProfileSide
