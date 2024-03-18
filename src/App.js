f5cf83aa196732fb53747c6a39db63e95c8a373c
HimashaNethmini <himashanethmini05@gmail.com>
2024-03-18 10:12:14 +0530

    design the blue filter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+      {/* design the blue filter  */}
+        <div className="blur" styles={{top: '--18%',right: '0'}}></div>
+        <div className="blur" styles={{top: '36%',right: '--8rem'}}></div>
     </div>
   );
 }
