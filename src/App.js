edd9657b850a7dc82fc7319386946dd725ecec75
HimashaNethmini <himashanethmini05@gmail.com>
2024-03-18 10:34:08 +0530

    imported home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
+import Home from "./pages/home/Home";
 function App() {
   return (
     <div className="App">
       {/* design the blue filter  */}
         <div className="blur" style={{top: '--18%',right: '0'}}></div>
         <div className="blur" style={{top: '36%',right: '--8rem'}}></div>
+        <Home />
     </div>
   );
 }
