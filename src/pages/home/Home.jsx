3b89fb3935aee874a29cdd41c7ee590edfcb41f8
HimashaNethmini <himashanethmini05@gmail.com>
2024-03-18 10:34:34 +0530

    divide the sections

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./home.css"
+import ProfileSide from "../profileSide/ProfileSide"
+
+const Home = () => {
+  return (
+    <div className='Home'>
+        <ProfileSide />
+        <div className="postSide">podt</div>
+        <div className="Rightside">right</div>
+
+      
+    </div>
+  )
+}
+
+export default Home
